18f5aaacb3ad9996de6ef6a7c622bfea4fa52c97
Eduardo Catalán <3026773130104@ingenieria.usac.edu.gt>
2022-07-01 22:27:31 -0600

    Regresion lineal terminada

--- MachineLearning_web.py
+++ MachineLearning_web.py
@@ -1,7 +1,14 @@
+from operator import mod
+from statistics import mode
 import streamlit as st
 import pandas as pd
 import os
- 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression;
+from sklearn.metrics import mean_squared_error, r2_score;
+from PIL import Image
+
 #Asignamos un titulo
 st.title('Machine Learning')
 
@@ -24,25 +31,53 @@ if uploaded_file is not None:
     nombre_archivo = split_tup[0]
     extension = split_tup[1]
 
-    st.write(extension)
-    
     if (extension == '.csv'):
         df = pd.read_csv(uploaded_file)
     elif(extension == '.xlsx'):
         df = pd.read_excel(uploaded_file)
-    
-    st.write("Nombre del archivo:",uploaded_file.name)
-    
-    
-#try:
+      
+try:
     st.write(df)
+    headers = df.head()
     if(model == 'Regresión lineal'):
-        st.write('Ejecutar regresion lineal mijo')
+        st.write('PARAMETRIZACION')
+        parameter_x = st.selectbox('Seleccione el parametro para el eje x',headers.columns)
+        parameter_y = st.selectbox('Seleccione el parametro para el eje y',headers.columns)
+        lin_reg = LinearRegression()
+        #Preparar datos
+        x_axis = df[parameter_x].values.reshape((-1,1)) #convertimos en un vector de vectores, en vez de un vector de datos
+        y_axis = df[parameter_y]
+        modelX = lin_reg.fit(x_axis,y_axis) #Ajustamos una linea a los datos que le daremos
+
+        #Prediccion de tendencias
+        y_pred = modelX.predict(x_axis)
+        st.write('PREDICCION DE TENDENCIA Y FUNCION DE TENDENCIA LINEAL',y_pred)
+        #Grafico de puntos de dispersion
+        st.write('GRAFICO DE PUNTOS Y FUNCION DE TENDENCIA LINEAL')
+        plt.scatter(x_axis,y_axis)
+        plt.plot(x_axis,y_pred,color='red')
+        plt.ylabel(parameter_x)
+        plt.xlabel(parameter_y)
+        plt.savefig('lin_reg.png')
+        plt.close()
+        image = Image.open('lin_reg.png')
+        st.image(image, caption='Grafico de tendencia')
+        st.write('CARACTERISTICAS')
+        #Características
+        st.write('Intersección (b) ', modelX.intercept_)
+        st.write('Pendiente (m) ', modelX.coef_[0])
+        st.write('Error medio: ', mean_squared_error(y_axis,y_pred,squared=True))
+        st.write('R^2: ',r2_score(y_axis,y_pred))
+        #Prediccion
+        st.write('PREDICCION ESPECIFICA')
+        number_input = st.number_input('Insertar número parapredicción de tendencia específica')
+        st.write('Prediccion : ',modelX.predict([[number_input]])[0])
+
     elif(model == 'Regresión polinomial'):
         st.write('la del polinomio')
-#except Exception as e:
-#    print(e)
-#    st.write("Por favor subir archivo para poder ejecutar el algoritmo")
+except Exception as e:
+    print(e)
+    st.write("Por favor subir archivo para poder ejecutar el algoritmo")
 
 
 
