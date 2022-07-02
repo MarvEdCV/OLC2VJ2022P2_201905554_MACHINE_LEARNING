1f40d86f95340feb9789ceb07dd59431adb563cd
Eduardo Catalán <3026773130104@ingenieria.usac.edu.gt>
2022-07-02 01:25:32 -0600

    Add regresion polinomial

--- MachineLearning_web.py
+++ MachineLearning_web.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression;
 from sklearn.metrics import mean_squared_error, r2_score;
 from PIL import Image
@@ -43,14 +44,14 @@ try:
         st.write('PARAMETRIZACION')
         parameter_x = st.selectbox('Seleccione el parametro para el eje x',headers.columns)
         parameter_y = st.selectbox('Seleccione el parametro para el eje y',headers.columns)
-        lin_reg = LinearRegression()
         #Preparar datos
-        x_axis = df[parameter_x].values.reshape((-1,1)) #convertimos en un vector de vectores, en vez de un vector de datos
+        x_axis = np.asarray(df[parameter_x]).reshape((-1,1)) #convertimos en un vector de vectores, en vez de un vector de datos
         y_axis = df[parameter_y]
-        modelX = lin_reg.fit(x_axis,y_axis) #Ajustamos una linea a los datos que le daremos
+        lin_reg = LinearRegression()
+        lin_reg.fit(x_axis,y_axis) #Ajustamos una linea a los datos que le daremos
 
         #Prediccion de tendencias
-        y_pred = modelX.predict(x_axis)
+        y_pred = lin_reg.predict(x_axis)
         st.write('PREDICCION DE TENDENCIA Y FUNCION DE TENDENCIA LINEAL',y_pred)
         #Grafico de puntos de dispersion
         st.write('GRAFICO DE PUNTOS Y FUNCION DE TENDENCIA LINEAL')
@@ -64,17 +65,56 @@ try:
         st.image(image, caption='Grafico de tendencia')
         st.write('CARACTERISTICAS')
         #Características
-        st.write('Intersección (b) ', modelX.intercept_)
-        st.write('Pendiente (m) ', modelX.coef_[0])
+        st.write('Intersección (b) ', lin_reg.intercept_)
+        st.write('Pendiente (m) ', lin_reg.coef_[0])
         st.write('Error medio: ', mean_squared_error(y_axis,y_pred,squared=True))
         st.write('R^2: ',r2_score(y_axis,y_pred))
         #Prediccion
         st.write('PREDICCION ESPECIFICA')
         number_input = st.number_input('Insertar número parapredicción de tendencia específica')
-        st.write('Prediccion : ',modelX.predict([[number_input]])[0])
-
+        st.write('Prediccion : ',lin_reg.predict([[number_input]])[0])
     elif(model == 'Regresión polinomial'):
-        st.write('la del polinomio')
+        st.write('PARAMETRIZACION')
+        parameter_x = st.selectbox('Seleccione el parametro para el eje x',headers.columns)
+        parameter_y = st.selectbox('Seleccione el parametro para el eje y',headers.columns)
+       
+        #Preparar datos
+        x_axis = np.asarray(df[parameter_x]).reshape((-1,1)) #convertimos en un vector de vectores, en vez de un vector de datos
+        y_axis = df[parameter_y]
+        #Grado del polinomio
+        number_degree = st.number_input('Insertar número grado de polinomio',min_value=2)
+        poly = PolynomialFeatures(degree=int(number_degree))
+        x_trans = poly.fit_transform(x_axis)
+      
+        lin_reg = LinearRegression()
+        lin_reg.fit(x_trans,y_axis) #Ajustamos una parabola a los datos que le daremos
+
+        #Prediccion de tendencias
+        y_pred = lin_reg.predict(x_trans)
+        st.write('PREDICCION DE TENDENCIA Y FUNCION DE TENDENCIA POLINOMIAL',y_pred)
+        #Grafico de puntos de dispersion
+        st.write('GRAFICO DE PUNTOS Y FUNCION DE TENDENCIA LINEAL')
+        plt.scatter(x_axis,y_axis)
+        plt.plot(x_axis,y_pred,color='red')
+        plt.ylabel(parameter_x)
+        plt.xlabel(parameter_y)
+        plt.savefig('pol_reg.png')
+        plt.close()
+        image = Image.open('pol_reg.png')
+        st.image(image, caption='Grafico de tendencia')
+        st.write('CARACTERISTICAS')
+        #Características
+        st.write('Error medio: ', np.sqrt(mean_squared_error(y_axis,y_pred,squared=True)))
+        st.write('R^2: ',r2_score(y_axis,y_pred))
+        #Prediccion
+        st.write('PREDICCION ESPECIFICA')
+        number_input = st.number_input('Insertar número parapredicción de tendencia específica')
+        x_new_min = int(number_input)
+        x_new_max = int(number_input)
+        x_new = np.linspace(x_new_min,x_new_max,1)
+        x_new = x_new[:,np.newaxis]
+        x_trans = poly.fit_transform(x_new)
+        st.write('Prediccion : ',lin_reg.predict(x_trans))
 except Exception as e:
     print(e)
     st.write("Por favor subir archivo para poder ejecutar el algoritmo")
